507562f4b7fafafdeaed3431f84ecab87d17042c
Muffin5671 <213143890+Muffin5671@users.noreply.github.com>
2026-05-19 14:25:11 -0400

    update

--- main.js
+++ main.js
@@ -2,18 +2,42 @@ let lvlXML;
 let lvlName;
 let lvlData;
 
+// b64 and gzip stolen from colon loollol
+// i think he only had the b64 function to give support to older browsers
 function b64Decode(str) { return Base64.atob(str.replace(/_/g, '/').replace(/-/g, '+')) }
 function b64Encode(str) { return Base64.btoa(str).replace(/\//g, '_').replace(/\+/g, '-') }
+
+function gzipDecode(str) {
+  let toDecode = b64Decode(str, true)
+  toDecode = new Uint8Array(toDecode.split('').map(x => x.charCodeAt(0)))
+  return pako.inflate(toDecode, {to: 'string'})
+}
 
 async function getLvlData() {
   const lvlXML = new DOMParser().parseFromString(await $("#file")[0].files[0].text(), "text/xml");
 
   lvlName = lvlXML.getElementsByTagName("s")[0].innerHTML;
-  lvlData = lvlXML.getElementsByTagName("s")[1].innerHTML;
+  lvlData = gzipDecode(lvlXML.getElementsByTagName("s")[1].innerHTML);
 
   return true;
  }
 
-$("#download")[0].addEventListener("click", () => {
-  getLvlData();
+$("#download")[0].addEventListener("click", async () => {
+  await getLvlData();
+
+  let lvlJSON = `{
+  "name": "${lvlName}",
+  "objects": [`;
+
+  const split = lvlData.split(";");
+  split.shift();
+  split.forEach(obj => {
+    let oS = obj.split(",");
+    let objJSON = `,\n    {"obj": ${oS[1]}, "x": ${oS[3]}, "y": ${oS[5]}}`;
+    lvlJSON = lvlJSON + objJSON;
+  });
+  lvlJSON = lvlJSON + "\n  ]\n}";
+
+  let JSONFile = new File([lvlJSON], `${lvlName}.json`);
+  saveAs(JSONFile);
 });
