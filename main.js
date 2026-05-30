fee8ed72a53f93b8f05fb5ebe1edde2d3459a689
Muffin5671 <213143890+Muffin5671@users.noreply.github.com>
2026-05-30 13:39:15 -0400

    nvm we aren't done

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 let lvlXML;
 let lvlName;
 let lvlData;
+// let lvlID;
 
 // b64 and gzip stolen from colon loollol
 // i think he only had the b64 function to give support to older browsers
@@ -13,30 +14,41 @@ function gzipDecode(str) {
   return pako.inflate(toDecode, {to: 'string'})
 }
 
-async function getLvlData() {
-  lvlXML = new DOMParser().parseFromString(await $("#file")[0].files[0].text(), "text/xml");
+async function getLvlData(type) {
+  switch (type) {
+    case 0:
+      lvlData = gzipDecode($("#textarea")[0].value);
+      break;
+    case 1:
+      lvlXML = new DOMParser().parseFromString(await $("#file")[0].files[0].text(), "text/xml");
+      break;
+  }
 
-  lvlName = lvlXML.getElementsByTagName("s")[0].innerHTML;
+  if (type == 1) lvlName = lvlXML.getElementsByTagName("s")[0].innerHTML
+  else lvlName = "Unknown";
+  // lvlID = Number(lvlXML.getElementsByTagName("i")[0].innerHTML);
 
-  let element;
-  let i = 0;
-  do {
-    element = lvlXML.getElementsByTagName("s")[i];
-    console.log(element);
-    i++;
-  } while (lvlXML.getElementsByTagName("s")[i].innerHTML.startsWith("H4sIA"))
+  if (type == 1) {
+    let element;
+    let i = 0;
+    do {
+      element = lvlXML.getElementsByTagName("s")[i];
+      i++;
+    } while (lvlXML.getElementsByTagName("s")[i].innerHTML.startsWith("H4sIA"))
 
-  lvlData = gzipDecode(element.innerHTML);
+    lvlData = gzipDecode(element.innerHTML);
+  }
 
   return true;
 }
 
-$("#download")[0].addEventListener("click", async () => {
+async function convertLvltoJSON(type) {
   try {
-    await getLvlData();
+    await getLvlData(type);
 
     let lvlJSON = {
       name: lvlName,
+      // id: lvlID,
       objects: []
     };
 
@@ -45,11 +57,13 @@ $("#download")[0].addEventListener("click", async () => {
 
     for (let i = 0; i < (split.length - 1); i++) {
       let oS = split[i].split(",");
-      lvlJSON.objects.push({
+      let objJSON = {
         obj: Number(oS[1]), 
         x: Number(oS[3]), 
         y: Number(oS[5]) 
-      });
+      };
+      
+      lvlJSON.objects.push(objJSON);
     }
 
     let JSONFile = new File([JSON.stringify(lvlJSON, undefined, 2)], `${lvlName}.json`);
@@ -57,4 +71,12 @@ $("#download")[0].addEventListener("click", async () => {
   } catch (e) {
     alert(`There was an error converting the file:\n\n${e}`);
   }
+}
+
+$("#download")[0].addEventListener("click", () => {
+  convertLvltoJSON(1);
+});
+
+$("#download2")[0].addEventListener("click", () => {
+  convertLvltoJSON(0);
 });
